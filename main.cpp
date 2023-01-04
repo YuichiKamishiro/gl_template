4bec6d292ace7828982ca3590f1823ec0b1ae7c8
YuichiKamishiro <yuichikamishiro@gmail.com>
2023-01-04 22:21:21 +0300

    vbo vao

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 std::string LoadFile(std::string fileName) {
     std::string bufferStr, contentStr;
@@ -58,13 +59,40 @@ int main() {
     glfwMakeContextCurrent(window);
     gladLoadGL();
     
-    GLuint program = CreateProgram("", "");
+    GLuint program = CreateProgram("shader.frag", "shader.vert");
     glUseProgram(program);
+    
+    GLuint vbo {};
+    glCreateBuffers(1, &vbo);
+
+    struct vertex {
+        float x, y, z;
+        float r, g, b;
+    };
+
+    const std::vector<vertex> vertices {
+        {-1.0f, -1.0f, 1.0f, 1.0f, 0.0f, 0.0f }, // Vertex 1
+        { 0.0f,  1.0f, 1.0f, 0.0f, 1.0f, 0.0f }, // Vertex 2
+        { 1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f }  // Vertex 3
+    };
+
+    glNamedBufferStorage(vbo, vertices.size() * sizeof(vertex), vertices.data(), 0);
+    
+    GLuint vao {};
+    glCreateVertexArrays(1, &vao);
+    glVertexArrayVertexBuffer(vao, 0, vbo, 0, sizeof(vertex));
+    glVertexArrayAttribBinding(vao, 0, 0);
+    glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float));
+    glVertexArrayBindingDivisor(vao, 0, 0);
+    glEnableVertexArrayAttrib(vao, 0);
 
     while(!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.1, 0.1, 0.2, 1);
-            
+           
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
         glfwSwapBuffers(window);
     }
 }
